b6763626fdc1b9fb36de266c90638ac8077b9850
root <root@ip-172-31-42-126.ap-south-1.compute.internal>
2023-11-27 06:06:33 +0000

    Commit message-2023-11-27 06:06

---
+++ spring-server/src/main/java/com/persistent/model/Pet.java
@@ -0,0 +1,40 @@
+package com.persistent.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import javax.annotation.Generated;
+
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+/**
+ * Pet
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-11-27T06:06:31.537Z[UTC]")
+@Data
+public class Pet {
+
+  private Long id;
+  
+  private String name;
+  
+  private String breed;
+  
+  private Long age;
+  
+  private Long price;
+  
+
+
+}
