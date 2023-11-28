060671b3fba5fb65dcdbd8de3d29a0cc3595b0ac
root <root@ip-172-31-42-126.ap-south-1.compute.internal>
2023-11-28 10:10:29 +0000

    APITesting generated file - 2023-11-28 10:10

--- swagger/APITest.java
+++ swagger/APITest.java
@@ -1,113 +1,111 @@
 import io.restassured.RestAssured;
 import io.restassured.authentication.AuthenticationScheme;
-import io.restassured.authentication.BasicAuthScheme;
 import io.restassured.http.ContentType;
-import org.junit.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class APIEndpointValidationTest {
+    
+    @BeforeClass
+    public void setBaseURI() {
+        RestAssured.baseURI = "https://reqres.in/api";
+    }
 
     @Test
     public void testGetUser() {
-        RestAssured.baseURI = "https://reqres.in/api";
-        given()
-                .when()
-                .get("/users/2")
-                .then()
-                .assertThat()
-                .statusCode(200)
-                .body("data.first_name", equalTo("Janet"));
+        given().
+        when().
+            get("/users/2").
+        then().
+            assertThat().
+                statusCode(200).
+                body("data.first_name", equalTo("Janet"));
     }
 
     @Test
     public void testCreateUser() {
-        RestAssured.baseURI = "https://reqres.in/api";
-        given()
-                .contentType(ContentType.JSON)
-                .body("{\"name\": \"Sam\", \"job\": \"Project Leader\"}")
-                .when()
-                .post("/users")
-                .then()
-                .assertThat()
-                .statusCode(201)
-                .body("name", equalTo("Sam"))
-                .body("job", containsString("Leader"));
+        given().
+            contentType(ContentType.JSON).
+            body("{\"name\": \"Sam\", \"job\": \"Project Leader\"}").
+        when().
+            post("/users").
+        then().
+            assertThat().
+                statusCode(201).
+                body("name", equalTo("Sam")).
+                body("job", containsString("Leader"));
     }
 
     @Test
     public void testUpdateUser() {
-        RestAssured.baseURI = "https://reqres.in/api";
-        given()
-                .contentType(ContentType.JSON)
-                .body("{\"name\": \"Isha\", \"job\": \"Software Engineer\"}")
-                .when()
-                .put("/users/2")
-                .then()
-                .assertThat()
-                .statusCode(200);
+        given().
+            contentType(ContentType.JSON).
+            body("{\"name\": \"Isha\", \"job\": \"Software Engineer\"}").
+        when().
+            put("/users/2").
+        then().
+            assertThat().
+                statusCode(200);
     }
 
     @Test
     public void testDeleteUser() {
-        RestAssured.baseURI = "https://reqres.in/api";
-        given()
-                .when()
-                .delete("/users/2")
-                .then()
-                .assertThat()
-                .statusCode(204);
+        given().
+        when().
+            delete("/users/2").
+        then().
+            assertThat().
+                statusCode(204);
     }
 
     @Test
     public void testGetUsersWithQueryParams() {
-        RestAssured.baseURI = "https://reqres.in/api";
-        given()
-                .param("page", 2)
-                .when()
-                .get("/users")
-                .then()
-                .assertThat()
-                .statusCode(200)
-                .body("data", not(empty()));
+        given().
+            queryParam("page", "2").
+        when().
+            get("/users").
+        then().
+            assertThat().
+                statusCode(200).
+                body("data", not(empty()));
     }
 
     @Test
-    public void testGetUserWithPathVariable() {
-        RestAssured.baseURI = "https://reqres.in/api";
-        given()
-                .pathParam("id", 3)
-                .when()
-                .get("/users/{id}")
-                .then()
-                .assertThat()
-                .statusCode(200)
-                .body("data.last_name", equalTo("Wong"));
+    public void testGetUserWithPathParams() {
+        given().
+            pathParam("id", "3").
+        when().
+            get("/users/{id}").
+        then().
+            assertThat().
+                statusCode(200).
+                body("data.last_name", equalTo("Wong"));
     }
 
     @Test
-    public void testRegisterUserWithBasicAuth() {
-        RestAssured.baseURI = "https://reqres.in/api";
-        AuthenticationScheme authScheme = new BasicAuthScheme().builder().username("username").password("password").build();
-        given()
-                .auth().basic("username", "password")
-                .when()
-                .post("/register")
-                .then()
-                .assertThat()
-                .statusCode(400);
+    public void testRegisterWithBasicAuth() {
+        given().
+            auth().
+            preemptive().
+            basic("username", "password").
+        when().
+            post("/register").
+        then().
+            assertThat().
+                statusCode(400);
     }
 
     @Test
-    public void testGetNonExistentUser() {
-        RestAssured.baseURI = "https://reqres.in/api";
-        given()
-                .pathParam("id", 4)
-                .when()
-                .get("/users/{id}")
-                .then()
-                .assertThat()
-                .statusCode(404);
+    public void testGetNonExistingUser() {
+        given().
+            pathParam("id", "4").
+        when().
+            get("/users/{id}").
+        then().
+            assertThat().
+                statusCode(404);
     }
 }
