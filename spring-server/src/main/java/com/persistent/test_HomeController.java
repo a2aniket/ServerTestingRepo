01470e5c2d2d1d8ab1f66ef3919e64c9d8e2f048
Anurag Jain <anurag_jain1@persistent.com>
2023-11-28 14:30:39 +0530

    TestGen generated unit tests - 2023-11-28 14:30

--- spring-server/src/main/java/com/persistent/test_HomeController.java
+++ spring-server/src/main/java/com/persistent/test_HomeController.java
@@ -1,17 +1,24 @@
 import org.junit.Test;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import static org.junit.Assert.assertEquals;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@Controller
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
 public class HomeControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    public void testIndex() {
-        HomeController homeController = new HomeController();
-        String result = homeController.index();
-        assertEquals("redirect:swagger-ui.html", result);
+    public void testIndexRedirect() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("swagger-ui.html"));
     }
+
 }
--- spring-server/src/main/java/com/persistent/test_SpringDocConfiguration.java
+++ spring-server/src/main/java/com/persistent/test_SpringDocConfiguration.java
@@ -1,33 +1,14 @@
-package com.persistent.configuration;
-
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import io.swagger.v3.oas.models.OpenAPI;
-
-public class SpringDocConfigurationTest {
-
-    @Test
-    public void testApiInfo() {
-        SpringDocConfiguration springDocConfiguration = new SpringDocConfiguration();
-        OpenAPI openAPI = springDocConfiguration.apiInfo();
-        assertNotNull(openAPI.getInfo().getTitle());
-        assertNotNull(openAPI.getInfo().getDescription());
-        assertNotNull(openAPI.getInfo().getVersion());
-        assertNotNull(openAPI.getInfo().getLicense());
-        assertNotNull(openAPI.getComponents());
-        assertNotNull(openAPI.getComponents().getSecuritySchemes().get("api_key"));
-        assertNotNull(openAPI.getComponents().getSecuritySchemes().get("petstore_auth"));
-    }
-
-    @Test
-    public void testSpringDocConfiguration() {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringDocConfiguration.class);
-        assertNotNull(context.getBean("com.persistent.configuration.SpringDocConfiguration.apiInfo"));
-        assertTrue(context.getBeanDefinitionCount() > 0);
-    }
-
+@Test
+public void testApiInfo() {
+    OpenAPI apiInfo = new SpringDocConfiguration().apiInfo();
+    assertNotNull(apiInfo);
+    assertEquals("Swagger Petstore - OpenAPI 3.0", apiInfo.getInfo().getTitle());
+    assertEquals("This is a sample Pet Store Server based on the OpenAPI 3.0 specification.  You can find out more about Swagger at [https://swagger.io](https://swagger.io). In the third iteration of the pet store, we've switched to the design first approach! You can now help us improve the API whether it's by making changes to the definition itself or to the code. That way, with time, we can improve the API in general, and expose some of the new features in OAS3.", apiInfo.getInfo().getDescription());
+    assertEquals("Apache 2.0", apiInfo.getInfo().getLicense().getName());
+    assertEquals("http://www.apache.org/licenses/LICENSE-2.0.html", apiInfo.getInfo().getLicense().getUrl());
+    assertEquals("1.0.11", apiInfo.getInfo().getVersion());
+    assertEquals(SecurityScheme.Type.APIKEY, apiInfo.getComponents().getSecuritySchemes().get("api_key").getType());
+    assertEquals(SecurityScheme.In.HEADER, apiInfo.getComponents().getSecuritySchemes().get("api_key").getIn());
+    assertEquals("api_key", apiInfo.getComponents().getSecuritySchemes().get("api_key").getName());
+    assertEquals(SecurityScheme.Type.OAUTH2, apiInfo.getComponents().getSecuritySchemes().get("petstore_auth").getType());
 }
